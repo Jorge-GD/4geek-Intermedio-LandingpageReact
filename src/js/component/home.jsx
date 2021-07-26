043f3c6c8fa427c53e99091e8cadfbbabac9d97c
BryanTello <bryanbat65@gmail.com>
2021-07-26 14:33:59 +0000

    add cards

---
+++ src/js/component/home.jsx
@@ -0,0 +1,14 @@
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import React from "react";
+import Carta from "./carta.jsx";
+
+//create your first component CARD
+export function Home() {
+	return (
+		<div className="text-center mt-5">
+			<Carta />
+		</div>
+	);
+}
+// cards......
