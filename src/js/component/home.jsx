2542385fac6e8ddf927727d24a9926708ed2c6fa
ricardorsario <ricardorsario@gmail.com>
2021-07-26 13:15:10 +0000

    Add Carousel

---
+++ src/js/component/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+//import Ricardo's Carousel component
+import CarouselRicardo from "./carouselRicardo.jsx";
+
+//create your first component
+export function Home() {
+	return (
+		<div>
+			<CarouselRicardo />
+		</div>
+	);
+}
